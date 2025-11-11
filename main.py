3631810fab3a419e95cf7f710dead87d9301cf4b
happy9ph <happyneph12@gmail.com>
2025-11-11 14:28:11 +0200

    Wina bwangu v9

--- main.py
+++ main.py
@@ -490,4 +490,7 @@ def view_commande(commande_id: int, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
